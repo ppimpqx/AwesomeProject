47f883c990cd6e94ed050ad419c2abf7c348d92b
ppimpqx <pimpakarnpimkt@gmail.com>
2024-04-03 16:01:01 +0700

    fix

--- navigations/RootStack.js
+++ navigations/RootStack.js
@@ -2,8 +2,13 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import BottomTab from "./BottomTab";
 import PokemonTab from "./PokemonTab";
+import Book from "../screens/week9/Book";
+import BookDetail from "../screens/week9/BookDetail";
+import BookForm from "../screens/week9/BookForm";
 import YourHealth from "../screens/week9/YourHealth";
 import TodoList from "../screens/week9/TodoList";
+import Location from "../screens/week10/Location";
+import LocationQuiz from "../screens/week10/LocationQuiz";
 
 const Stack = createStackNavigator();
 
@@ -12,8 +17,13 @@ export default function RootStack() {
     <Stack.Navigator initialRouteName="BottomTab">
       <Stack.Screen name="BottomTab" component={BottomTab} options={{ headerShown: false }} />
       <Stack.Screen name="PokemonTab" component={PokemonTab} options={{ title : "Pokemon World" }}  />
+      <Stack.Screen name="Book" component={Book} options={{ title: "Book" }} />
+      <Stack.Screen name="BookDetail" component={BookDetail} options={{ title: "Book Detail" }} />
+      <Stack.Screen name="BookForm" component={BookForm}   />
       <Stack.Screen name="YourHealth" component={YourHealth} />
       <Stack.Screen name="TodoList" component={TodoList} />
+      <Stack.Screen name="Location" component={Location} />
+      <Stack.Screen name="LocationQuiz" component={LocationQuiz} />
     </Stack.Navigator>
   );
 }
